f0c344f655f54ea69e5362a80d96413d59404a7e
Gautham Chettiar <gauthamchettiar@gmail.com>
2025-11-13 10:38:23 +0530

    feat: adding copy button to codeblock

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,30 @@
     initTheme();
   }
 })();
+
+// Copy code functionality
+(function() {
+  function initCodeCopy() {
+    document.querySelectorAll('.codeblock-copy').forEach(button => {
+      button.addEventListener('click', function() {
+        const codeblock = this.closest('.codeblock');
+        const code = codeblock.querySelector('pre code');
+        if (code) {
+          navigator.clipboard.writeText(code.textContent).then(() => {
+            const originalHTML = this.innerHTML;
+            this.innerHTML = '<svg class="icon-copy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
+            setTimeout(() => {
+              this.innerHTML = originalHTML;
+            }, 2000);
+          });
+        }
+      });
+    });
+  }
+  
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initCodeCopy);
+  } else {
+    initCodeCopy();
+  }
+})();
